8d0d16cce1ba924517230919403f89f147799a14
ahamadey27 <hamadey@gmail.com>
2025-04-15 11:19:45 -0400

    Updated CRUD / Dependency Code with Comments

--- 03_BackendDevelopmentWithNET/CrudWebApiExercise/CrudWebApiExercise/obj/Debug/net9.0/CrudWebApiExercise.AssemblyInfo.cs
+++ 03_BackendDevelopmentWithNET/CrudWebApiExercise/CrudWebApiExercise/obj/Debug/net9.0/CrudWebApiExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudWebApiExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2213ea3e5f2f0198d9c606519e3faedd08af1daf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudWebApiExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudWebApiExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
