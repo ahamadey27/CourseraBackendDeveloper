a5ee4af9e44f12d62ae7974745ab6e296695e321
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-05-07 11:10:00 -0400

    SQL Push 1

--- 04_DatabaseIntegration/CRUDWithMySQL/CRUDWithMySQL/obj/Debug/net9.0/CRUDWithMySQL.AssemblyInfo.cs
+++ 04_DatabaseIntegration/CRUDWithMySQL/CRUDWithMySQL/obj/Debug/net9.0/CRUDWithMySQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDWithMySQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d16e8e27e7f7e284fcc0d1123a7bbbf03deb447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67ffd7a33865026f48f2b3ce99588c4a4070b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDWithMySQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDWithMySQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
